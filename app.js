827e8a7bdba45f88a0bef93910560dcb882ef59b
Morton358 <vovankovalchuk358@gmail.com>
2018-04-08 16:59:10 +0300

    created handling of username from frontend

--- app.js
+++ app.js
@@ -5,53 +5,86 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-const params = { screen_name: 'voloyev', count: 3 };
-let tweets = null;
-// function rawurlencode(str) {
-//     return encodeURIComponent(str)
-//         .replace(/!/g, '%21')
-//         .replace(/'/g, '%27')
-//         .replace(/\(/g, '%28')
-//         .replace(/\)/g, '%29')
-//         .replace(/\*/g, '%2A');
-// }
-// BASE64_BEARER_TOKEN_CREDENTIALS
-// const CONSUMER_KEY_RFC1738 = rawurlencode(config.CONSUMER_KEY);
-// const CONSUMER_SECRET_RFC1738 = rawurlencode(config.CONSUMER_SECRET);
-// const ACCESS_TOKEN_KEY_RFC1738 = rawurlencode(config.ACCESS_TOKEN_KEY);
-// const ACCESS_TOKEN_SECRET_RFC1738 = rawurlencode(config.ACCESS_TOKEN_SECRET);
+// let tweets = null;
 //
-// const BEARER_TOKEN_CREDENTIALS =
-//     CONSUMER_KEY_RFC1738 + ':' + CONSUMER_SECRET_RFC1738;
-// const BASE64_BEARER_TOKEN_CREDENTIALS = Buffer.from(
-//     BEARER_TOKEN_CREDENTIALS
-// ).toString('base64');
+// const params = {
+//     screen_name: username,
+//     max_id: 977979599102595100,
+//     count: 4,
+//     trim_user: true,
+//     exclude_replies: true
+// }
+//
+// const client = new Twit({
+//     consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//     access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
+//     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+//     timeout_ms: 60 * 1000
+// });
+// client
+//     .get('statuses/user_timeline', params)
+//     .catch(error => {
+//         console.log('caught error', error.stack);
+//     })
+//     .then(result => {
+//         tweets = result.data;
+//         console.log('data', result.data);
+//     });
 
-const client = new Twit({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-    timeout_ms: 60 * 1000
-});
+const expressReqRes = async username => {
+    try {
+        const tweets = await getTweets(username);
+        console.log(tweets);
+        return tweets;
+    } catch (err) {
+        console.log(err);
+    }
+};
 
-client
-    .get('statuses/user_timeline', params)
-    .catch(error => {
-        console.log('caught error', error.stack);
-    })
-    .then(result => {
-        tweets = result.data
-        console.log('data', result.data);
+const getTweets = async username => {
+    const params = {
+        screen_name: username,
+        max_id: 977979599102595100,
+        count: 4,
+        trim_user: true,
+        exclude_replies: true
+    };
+
+    console.log(params);
+
+    const client = await new Twit({
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
+        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+        timeout_ms: 60 * 1000
     });
 
+    await client
+        .get('statuses/user_timeline', params)
+        .catch(error => {
+            console.log('caught error', error.stack);
+        })
+        .then(result => {
+            const tweets = result.data;
+            console.log('data', result.data);
+            return tweets;
+        });
+};
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
 });
-app.get('/', (req, res) => {
+
+app.get('/:username', async (req, res) => {
+    console.log(req.params.username);
+    const tweets = await expressReqRes(req.params.username);
     res.send(tweets);
 });
 
--- client/src/App.js
+++ client/src/App.js
@@ -6,34 +6,44 @@ import './App.css';
 
 class App extends Component {
     state = {
-        tweets: ''
+        tweets: '',
+        username: ''
     };
     componentDidMount() {
-        axios
-            .get('/')
-            .then(response => {
-                console.log(response.data);
-                // this.setState({tweets: response.data});
-            })
-            .catch( (error) => {
-                if (error.response) {
-                    // The request was made and the server responded with a status code
-                    // that falls out of the range of 2xx
-                    console.log(error.response.data);
-                    console.log(error.response.status);
-                    console.log(error.response.headers);
-                } else if (error.request) {
-                    // The request was made but no response was received
-                    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                    // http.ClientRequest in node.js
-                    console.log(error.request);
-                } else {
-                    // Something happened in setting up the request that triggered an Error
-                    console.log('Error', error.message);
-                }
-                console.log(error.config);
-            });
-    };
+
+    }
+
+    handleInputUsername = (event) => {
+        console.log(event.target.value);
+        this.setState({username: event.target.value})
+    }
+    handleSubmitForm = (event) => {
+        event.preventDefault();
+        if (this.state.username !== '') {
+            const usr = this.state.username.slice(1);
+            console.log(usr);
+            axios
+                .get(`/${usr}`)
+                .then(response => {
+                    console.log(response.data);
+                    // this.setState({tweets: response.data});
+                })
+                .catch(error => {
+                    if (error.response) {
+                        console.log(error.response.data);
+                        console.log(error.response.status);
+                    } else if (error.request) {
+                        console.log(error.request);
+                    } else {
+                        console.log('Error', error.message);
+                    }
+                    console.log(error.config);
+                });
+        } else {
+            console.log('Please write username');
+        }
+
+    }
 
     render() {
         return (
@@ -42,6 +52,26 @@ class App extends Component {
                     <img src={logo} className="App-logo" alt="logo" />
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
+                <form className="App-form">
+                    <div>
+                        <label htmlFor="username">Write username: </label>
+                        <input
+                            className="App-form__input"
+                            type="text"
+                            required
+                            placeholder="@voloyev"
+                            pattern="^@?(\w){1,15}$"
+                            onChange={this.handleInputUsername}
+                        />
+                        <p className="App-form__p">
+                            Usernames must be 1-15 characters in
+                            length, starting from '@' symbol
+                        </p>
+                    </div>
+                    <div>
+                        <button onClick={this.handleSubmitForm}>Submit</button>
+                    </div>
+                </form>
                 <p className="App-intro">{this.state.response}</p>
             </div>
         );
