a7d25f1c31d647b840e70f641d0808b9ae18ad7f
Morton358 <vovankovalchuk358@gmail.com>
2018-04-11 06:07:27 +0300

    startted of writing testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,32 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+//
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from './App';
+import Header from './components/Header/Header';
+import UsernameForm from './components/UsernameForm/UsernameForm';
+
+configure({ adapter: new Adapter() });
+
+describe('<App />', () => {
+    it('Should render <Header /> component', () => {
+        const wrapper = shallow(<App />);
+        exprect(wrapper.find(Header, UsernameForm)).toHaveLength(1);
+    });
+
+    it('Should render <UsernameForm /> components', () => {
+        const wrapper = shallow(<App />);
+        exprect(wrapper.find(Header, UsernameForm)).toHaveLength(1);
+    });
+
+
 });
